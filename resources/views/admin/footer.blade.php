8f0419caf5c0653cf02471642a90951315afef78
alvine2200 <alvinellavu@gmail.com>
2023-10-30 17:24:05 +0300

    refactured admin panel

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="main-footer">
+
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+
+    <strong>Copyright &copy; 2014-{{ now()->format('Y') }} <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All
+    rights
+    reserved.
+</footer>
