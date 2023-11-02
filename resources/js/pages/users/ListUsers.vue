6c28b416ffd5b8219960a9a283724dd895ded66a
alvine2200 <alvinellavu@gmail.com>
2023-11-02 11:41:26 +0300

    date formatting

--- resources/js/pages/users/ListUsers.vue
+++ resources/js/pages/users/ListUsers.vue
@@ -4,7 +4,7 @@ import { ref, onMounted, reactive } from "vue";
 import { Form, Field } from 'vee-validate';
 import * as yup from 'yup';
 import { useToastr } from '../../toastr.js';
-
+import { formatDate } from '../../helper.js';
 
 const toastr = useToastr();
 const users = ref([]);
@@ -164,7 +164,7 @@ onMounted(() => {
                   <td>{{ index + 1 }}</td>
                   <td>{{ user.name }}</td>
                   <td>{{ user.email }}</td>
-                  <td></td>
+                  <td>{{ formatDate(user.created_at) }}</td>
                   <td></td>
                   <td>
                     <a @click.prevent="editUser(user)"><i class="fa fa-edit mr-2"></i></a>
